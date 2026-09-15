9682c5afa4a671cc30c3f942d886384a8d6fdd93
Anorak097 <anorakyang@gmail.com>
2026-09-15 14:28:26 +0800

    修正下載連結，將原始檔案路徑更改為可直接訪問的 GitHub 網頁路徑

--- global-top.vue
+++ global-top.vue
@@ -16,7 +16,7 @@ const tocPage = () => {
         class="fixed top-2 right-2 z-50 flex gap-2"
     >
         <a 
-    href="https://github.com/anorak097/BLSH_Integrated_Basic_Tutorial_Manual/raw/main/Integrated%20Basic%20Tutorial%20Manual%20-%20Slidev.pdf"
+    href="https://github.com/anorak097/BLSH_Integrated_Basic_Tutorial_Manual/blob/main/Integrated%20Basic%20Tutorial%20Manual%20-%20Slidev.pdf"
     target="_blank"
     download
     class="inline-flex items-center justify-center p-2 rounded-full bg-gray-400 bg-opacity-10 hover:bg-opacity-20 border border-gray-500 border-opacity-20 backdrop-blur-md transition-all active:scale-90 !text-current"
